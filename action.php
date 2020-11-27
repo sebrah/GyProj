df6740cfbe501487db4c8d61fa46b8058157d22b
sebrah <se@entian.se>
2020-11-27 08:23:52 +0100

    Lade till action.php

---
+++ action.php
@@ -0,0 +1,32 @@
+<?php
+
+include "includes\IncludeHandling.php"; 
+
+session_start();
+
+if(!isset($_POST['form_submitted'])) {
+    header('dev.html');
+    exit();
+} else {
+    #$_SESSION['totalTime'] = time() - $_SESSION['startTime'];
+    #printCodeTime($_SESSION['totalTime']);
+    switch ($_POST['form_submitted']) {
+        case '1':
+            #mat
+            break;
+        case '2':
+            #dryck
+            break;
+        case '3':
+            #boka
+            break;
+        case '4':
+            #om-oss
+            break;
+        default:
+            header('dev.html');
+            break;
+    }
+}
+
+?>
