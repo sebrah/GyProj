f5e9c82659d9ef722338e885ffb85aecc86f056c
sebrah <se@entian.se>
2020-12-01 11:02:09 +0100

    edit

--- .php
+++ .php
@@ -2,6 +2,6 @@
 
 fwrite(fopen("dat\data", "a"), "[" . date("H:i:s") . "] @ " . $_SERVER['REMOTE_ADDR'] . " -> connected to html file\n");
 
-header('Location: dev.html');
+header('Location: dev-copy.html');
 
 ?>
