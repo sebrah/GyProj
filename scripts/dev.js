6f949b6142d56a8ddd88a91e38343cd4d3de0c74
sebrah <se@entian.se>
2021-01-21 22:13:56 +0100

    Developement 21/01

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,8 +1,40 @@
-document.getElementById("mat").addEventListener("click", function() {
-    window.location.href = "dev2.html";
-});
-document.getElementById("om").addEventListener("click", function() {
-    window.location.href = "om-oss.html";
-});
+try {
+    document.getElementById("mat").addEventListener("click", function() {
+        window.location.href = "mat.html";
+    });
+}
+catch (error) {
+    console.log("mat button not available on this page");
+}
+
+try {
+    document.getElementById("om").addEventListener("click", function() {
+        window.location.href = "om-oss.html";
+    });
+}
+catch (error) {
+    console.log("on button not available on this page");
+}
+
+try {
+    document.getElementById("title").addEventListener("click", function() {
+        window.location.href = "index.html";
+    });
+}
+catch (error) {
+    console.log("title button not available on this page");
+}
+
+try {
+    document.getElementById("lunch").addEventListener("click", function() {
+        window.location.href = "mat/lunch.html";
+    });
+    document.getElementById("middag").addEventListener("click", function() {
+        window.location.href = "mat/middag.html";
+    });
+}
+catch (error) {
+    console.log("menu buttons not available on this page");
+}
 
 //TODO: gör separata
--- scripts/dev2.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("title").addEventListener("click", function() {
-    window.location.href = "dev-copy.html";
-});
