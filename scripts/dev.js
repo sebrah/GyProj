4b1c8daf7687d43d941b96c4b32dca0f3ec095fd
sebrah <se@entian.se>
2021-01-23 22:10:48 +0100

    Edit 23/1

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,3 +1,5 @@
+
+
 try {
     document.getElementById("mat").addEventListener("click", function() {
         window.location.href = "mat.html";
@@ -13,13 +15,32 @@ try {
     });
 }
 catch (error) {
-    console.log("on button not available on this page");
+    console.log("om button not available on this page");
 }
 
 try {
-    document.getElementById("title").addEventListener("click", function() {
-        window.location.href = "index.html";
+    document.getElementById("boka").addEventListener("click", function() {
+        window.location.href = "boka.html";
     });
+}
+catch (error) {
+    console.log("boka button not available on this page");
+}
+
+try {
+    var title = document.getElementById("title");
+    if(document.getElementById("m")) {
+        document.getElementById("title").addEventListener("click", function() {
+            window.location.href = "../index.html";
+        });
+        console.log(0);
+    }
+    else {
+        document.getElementById("title").addEventListener("click", function() {
+            window.location.href = "index.html";
+        });
+        console.log(1);
+    }
 }
 catch (error) {
     console.log("title button not available on this page");
